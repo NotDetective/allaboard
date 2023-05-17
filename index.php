e89072c772cf3a1dedbcb16b9e31b653e616e03a
Woedroe <118165188+WoutLar@users.noreply.github.com>
2023-05-17 13:36:32 +0200

    index complete (-responsive) + begin tickets
    made header +footer + index fully with slideshow, top rated, deals and all the tickets (placeholders)

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
         echo"<div class='menu'>";
             for ($i=0; $i <= 4; $i++) { 
                 echo "<div class='toprated'>
-                            <h1>Top rated</h1>
+                            <h2>Top rated</h2>
                             <div></div>
                             <p></p>
                             <p></p>
@@ -26,15 +26,58 @@
             echo"<div class='menu'>";
             for ($i=0; $i <= 4; $i++) { 
                 echo "<div class='toprated'>
-                            <h1>Sales</h1>
+                            <h2>deals</h2>
                             <div></div>
                             <p></p>
                             <p></p>
                         </div>";
             }
-            echo "</div>"  ;
+            echo "</div>";
         ?>
+
+            <div class="containerss">
+                <div class="flexbox">
+                    <div class="overlayss">
+                        <div>test</div>
+                        <div>test</div>
+                        <div>test</div>
+                        <div>test</div>
+                        <div>test</div>
+                    </div>
+                </div>
+                <img src="img/trainImage1.png" alt="error404" class="ssimg" id="slideshowimg">
+            </div>
+
+            <div class="containerTicket">
+                <?php
+                // maak de "for", een "foreach" statement.
+                    for ($i=0; $i <= 19; $i++) { 
+                        echo"<div class='ticket'>
+                        
+                                <div class='imgholder'>
+                                    <img src='/#' alt='error404' class='img'>
+                                </div>
+                                <div class='ticketInfo'>
+                                    <h1 class='name'>name</h1>
+                                    <p class='description'>Lorem ipsum daolor sit amet, consectetur adipiscing elit. Pellentesque euismod accumsan hendrerit. Aliquam aliquet nisi sed rutrum elementum. Vivamus in.</p>
+                                    <p class='location'>date / location</p>
+                                    <p class='time'>time</p>
+                                </div>
+                                <div class='priceContainer'>
+                                    <p class='price'>price</p>
+                                    <div>
+                                        <a href='/#'></a>
+                                    </div>
+                                </div>
+
+                            </div>";
+                    }
+                ?>
+            </div>
+
     </main>
     <?php include'footer.php'; ?>
+
+    <script src="js/main.js"></script>
 </body>
 </html>
