42905b45dafe31f0004e113b7b7c8ed8d4cf640f
Woedroe <118165188+WoutLar@users.noreply.github.com>
2023-06-06 11:45:24 +0200

    login/register page done
    login/register page now has styling + some animation for better user experience (not responsive

--- login-and-register-page.php
+++ login-and-register-page.php
@@ -18,36 +18,66 @@
 
 <body>
 
-    <!-- <?php include 'header.php'; ?> -->
+    <?php include 'header.php'; ?>
 
-    <form name="login" action="pages/login.php" method="POST">
-        <input name="emailLogin" type="text" placeholder="Enter email">
+    <main>
+        <div class="login-and-register-title">
+            login/register
+        </div>
+        <div class="login-and-register-flexBox">
+            <div class="login-flexBox">
+                <div class="login-title">login</div>
+                <form name="login" action="pages/login.php" method="POST">
+                    <input name="emailLogin" type="text" placeholder="Enter email">
 
-        <input id="passwordLogin" name="passwordLogin" type="password" placeholder="Enter password">
-        <input type="checkbox" onclick="showPasswordLogin()"> Show Password
+                    <input id="passwordLogin" name="passwordLogin" type="password" placeholder="Enter password">
+                    
+                    <div class="checkbox">
+                        <input type="checkbox" onclick="showPasswordLogin()"> Show Password
+                    </div>
 
-        <button name="loginSubmit" type="submit">Login</button>
-    </form>
+                    <button name="loginSubmit" type="submit">Login</button>
+                </form>
+            </div>
+            <div class="register-flexBox">
+            <div class="register-title">register</div>
+                <form id="register-from" name="register" action="pages/register.php" method="POST" >
+                    <input id="emailRegister" name="emailRegister" type="email" placeholder="Enter email">
 
-    <form id="register-from" name="register" action="pages/register.php" method="POST" >
-        <input id="emailRegister" name="emailRegister" type="email" placeholder="Enter email">
+                    <input onchange="passwordRequirersCheck(this.id)" id="passwordRegister" name="passwordRegister" type="password" placeholder="Enter password">
+                    <div>
+                        <input type="checkbox" onclick="showPasswordRegister()"> Show Password
+                    </div>
 
-        <input onchange="passwordRequirersCheck(this.id)" id="passwordRegister" name="passwordRegister" type="password" placeholder="Enter password">
-        <input type="checkbox" onclick="showPasswordRegister()"> Show Password
+                    <button onclick="registerFunction()" id="registerSubmit" name="registerSubmit" type="submit">Register</button>
+                </form>
+            </div>
 
-        <button onclick="registerFunction()" id="registerSubmit" name="registerSubmit" type="submit">Register</button>
-    </form>
+        </div>
+        <div class="password-requirements-title">password requirements</div>
+        <div class="password-requirements">
+                <div>
+                    <p id="passwordRequirementsUpercase">Contains upercase letter</p>
+                </div>
+                <div>
+                    <p id="passwordRequirementsLowercase">Contains lowercase letter</p>
+                </div>
+                <div>
+                    <p id="passwordRequirementsNumber">Contains number</p>
+                </div>
+                <div>
+                    <p id="passwordRequirementsSpecialCharacter">Contains special character</p>
+                </div>
+                <div>
+                    <p id="passwordRequirementsLength">Password length is at least 8 characters long</p>
+                </div>
+            </div>
 
-    <div>
-        <p id="passwordRequirementsUpercase">Contains upercase letter</p>
-        <p id="passwordRequirementsLowercase">Contains lowercase letter</p>
-        <p id="passwordRequirementsNumber">Contains number</p>
-        <p id="passwordRequirementsSpecialCharacter">Contains special character</p>
-        <p id="passwordRequirementsLength">Password length is at least 8 characters long</p>
-    </div>
+
+    </main>
 
 
-    <!-- <?php include 'footer.php'; ?> -->
+    <?php include 'footer.php'; ?>
     <script src="js/account.js"></script>
     <script src="js/main.js"></script>
 </body>
