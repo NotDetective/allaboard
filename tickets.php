9d5ecfbfec7c37aa2571f422d2eae2c295e65a40
Woedroe <118165188+WoutLar@users.noreply.github.com>
2023-06-06 09:08:37 +0200

    customer use of tickets frontend done
    made the ticket page for the customer side with the information, img, price etc (not responsive yet)

---
+++ tickets.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>All Aboard!</title>
+    <link rel="stylesheet" href="css/ticket.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" href="/#" type="image/x-icon">
+</head>
+
+<body>
+    
+    <?php include 'header.php'; ?>
+
+    <main>
+        <div class="flexBox">
+            <div class="infoBox">
+                <div class="nameBox">
+                    <p>Name</p>
+                </div>
+                <p>Lorem ipsum daolor sit amet, consectetur adipiscing elit. 
+                    Pellentesque euismod accumsan hendrerit.
+                    Aliquam aliquet nisi sed rutrum elementum. 
+                    Vivamus in.</p>
+            </div>
+            <div class="infoBox2">
+                <div class="imgContainer">
+                    <img src="/#" alt="Image">
+                </div>
+                <div class="otherInformation">
+                    <div class="priceBox">
+                        <p>(placeholder)</p>
+                    </div>
+                    <div class="seats">
+                        <p>(placeholder)</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <?php include 'footer.php'; ?>
+</body>
